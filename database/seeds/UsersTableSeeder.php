7b753500202ca0b1218ddf8cab4b8ccdfd9776a1
off-ini <offridamjc@gmail.com>
2020-08-10 13:26:00 +0200

    init

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,7 +14,12 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'Infini',
+            'f_name' => 'Infini',
             'code' => 'IIIIIIII',
+            'phone' => '0000000000',
+            'adresse' => 'Lomé',
+            'is_actived' => true,
+            'ville_id' => 1,
             'email' => 'infini@gmail.com',
             'username' => 'infini',
             'password' => '25d55ad283aa400af464c76d713c07ad', //-> 12345678,
@@ -22,7 +27,12 @@ class UsersTableSeeder extends Seeder
 
         DB::table('users')->insert([
             'name' => 'Jean',
+            'f_name' => 'Jean',
             'code' => 'JJJJJJJJ',
+            'phone' => '0000000000',
+            'adresse' => 'Lomé',
+            'is_actived' => true,
+            'ville_id' => 1,
             'email' => 'jean@gmail.com',
             'username' => 'jean',
             'password' => '25d55ad283aa400af464c76d713c07ad', //-> 12345678,
@@ -30,7 +40,12 @@ class UsersTableSeeder extends Seeder
 
         DB::table('users')->insert([
             'name' => 'Claude',
+            'f_name' => 'Calude',
             'code' => 'CCCCCCCC',
+            'phone' => '0000000000',
+            'adresse' => 'Lomé',
+            'is_actived' => true,
+            'ville_id' => 1,
             'email' => 'claude@gmail.com',
             'username' => 'claude',
             'password' => '25d55ad283aa400af464c76d713c07ad', //-> 12345678,
