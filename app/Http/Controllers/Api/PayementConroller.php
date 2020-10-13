7f5b4fe0ecc6180b8d18fbb229912089b1bcb2c5
off-ini <offridamjc@gmail.com>
2020-10-13 03:42:52 +0200

    13/10/2020

--- app/Http/Controllers/Api/PayementConroller.php
+++ app/Http/Controllers/Api/PayementConroller.php
@@ -43,7 +43,7 @@ class PayementConroller extends Controller
             Mission::sendMessage($msg,$phone);
         }
 
-        return redirect('https://fretload.herokuapp.com/app/missions/paided');
+        return redirect('https://fretload.herokuapp.com/#/app/missions/paided');
     }
 
     /**
